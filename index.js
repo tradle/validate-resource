3a7d994a042bac91bc9b1f1e5517259f22f46020
Mark Vayngrib <mark@tradle.io>
2017-07-16 19:56:36 -0400

    add bytes type, export utils

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { TYPE } = require('./lib/constants')
 exports = module.exports = validateResource
 exports.resource = validateResource
 exports.property = require('./lib/property')
+const utils = require('./lib/utils')
+exports.utils = utils
 const validateLocal = require('./lib/resource')
 const validateRefs = require('./lib/refs')
-const { assert, toModelsMap } = require('./lib/utils')
+const { assert, toModelsMap } = utils
 
 function validateResource ({ model, models, resource }) {
   assert(typeof resource[TYPE] === 'string', `expected "${TYPE}"`)
