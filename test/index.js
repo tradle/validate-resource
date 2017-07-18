5b80f13a8afae356952aaa4f3b458992ec42b9d1
Mark Vayngrib <mark@tradle.io>
2017-07-18 12:25:30 -0400

    include _virtual in pickVirtual(), test

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const test = require('tape')
 const models = require('@tradle/models').models.concat(require('@tradle/custom-models'))
 // const Profile = models['tradle.Profile']
 const validate = require('../')
+const { utils } = validate
 const broken = require('./fixtures/invalid')
 const good = require('./fixtures/valid')
 
@@ -19,6 +20,39 @@ test('validate resource', function (t) {
       models,
       resource: item.resource
     }))
+  })
+
+  t.end()
+})
+
+test('virtual properties', function (t) {
+  const obj = {
+    a: 1
+  }
+
+  utils.setVirtual(obj, { _ha: 'ha' })
+  t.same(obj, {
+    a: 1,
+    _ha: 'ha',
+    _virtual: ['_ha']
+  })
+
+  utils.setVirtual(obj, { _hey: 'ho' })
+  t.same(obj, {
+    a: 1,
+    _ha: 'ha',
+    _hey: 'ho',
+    _virtual: ['_ha', '_hey']
+  })
+
+  t.same(utils.omitVirtual(obj), {
+    a: 1
+  })
+
+  t.same(utils.pickVirtual(obj), {
+    _ha: 'ha',
+    _hey: 'ho',
+    _virtual: ['_ha', '_hey']
   })
 
   t.end()
