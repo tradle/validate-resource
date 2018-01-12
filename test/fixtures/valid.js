e1ac9a5d3d58f226ec9cf0c2d90bfe0ed77f6d83
Mark Vayngrib <mark@tradle.io>
2018-01-11 22:58:48 -0500

    fix: inlined resource validation

--- test/fixtures/valid.js
+++ test/fixtures/valid.js
@@ -95,5 +95,183 @@ module.exports = [
       }
     },
     error: /required/
+  },
+  {
+    model: models['tradle.APIBasedVerificationMethod'],
+    resource: {
+      "_t": "tradle.APIBasedVerificationMethod",
+      "api": {
+        "_t": "tradle.API",
+        "name": "onfido"
+      },
+      "reference": [
+        {
+          "queryId": "report:954f62ef-3180-4711-929b-e74fa2397d5b"
+        }
+      ],
+      "aspect": "authenticity",
+      "rawData": {
+        "created_at": "2018-01-12T03:17:45Z",
+        "href": "/v2/checks/af009895-106b-4e47-a3d1-036156090dba/reports/954f62ef-3180-4711-929b-e74fa2397d5b",
+        "id": "954f62ef-3180-4711-929b-e74fa2397d5b",
+        "name": "document",
+        "properties": {
+          "last_name": "Soni145b",
+          "issuing_country": "GBR",
+          "first_name": "Moog",
+          "document_type": "passport",
+          "document_numbers": [
+            {
+              "value": "9999999999",
+              "type": "passport"
+            }
+          ],
+          "date_of_expiry": "1900-01-01",
+          "date_of_birth": "1900-01-01"
+        },
+        "result": "clear",
+        "status": "complete",
+        "sub_result": "clear",
+        "variant": "standard",
+        "breakdown": {
+          "data_comparison": {
+            "result": "clear",
+            "breakdown": {
+              "date_of_birth": {
+                "result": "clear",
+                "properties": {}
+              },
+              "date_of_expiry": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_numbers": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_type": {
+                "result": "clear",
+                "properties": {}
+              },
+              "first_name": {
+                "result": "clear",
+                "properties": {}
+              },
+              "gender": {
+                "result": "clear",
+                "properties": {}
+              },
+              "issuing_country": {
+                "result": "clear",
+                "properties": {}
+              },
+              "last_name": {
+                "result": "clear",
+                "properties": {}
+              }
+            }
+          },
+          "data_consistency": {
+            "result": "clear",
+            "breakdown": {
+              "date_of_birth": {
+                "result": "clear",
+                "properties": {}
+              },
+              "date_of_expiry": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_numbers": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_type": {
+                "result": "clear",
+                "properties": {}
+              },
+              "first_name": {
+                "result": "clear",
+                "properties": {}
+              },
+              "gender": {
+                "result": "clear",
+                "properties": {}
+              },
+              "issuing_country": {
+                "result": "clear",
+                "properties": {}
+              },
+              "last_name": {
+                "result": "clear",
+                "properties": {}
+              },
+              "nationality": {
+                "result": "clear",
+                "properties": {}
+              }
+            }
+          },
+          "data_validation": {
+            "result": "clear",
+            "breakdown": {
+              "date_of_birth": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_expiration": {
+                "result": "clear",
+                "properties": {}
+              },
+              "document_numbers": {
+                "result": "clear",
+                "properties": {}
+              },
+              "expiry_date": {
+                "result": "clear",
+                "properties": {}
+              },
+              "gender": {
+                "result": "clear",
+                "properties": {}
+              },
+              "mrz": {
+                "result": "clear",
+                "properties": {}
+              }
+            }
+          },
+          "visual_authenticity": {
+            "result": "clear",
+            "breakdown": {
+              "face_detection": {
+                "result": "clear",
+                "properties": {}
+              },
+              "other": {
+                "result": "clear",
+                "properties": {}
+              }
+            }
+          },
+          "image_integrity": {
+            "result": "clear",
+            "breakdown": {
+              "image_quality": {
+                "result": "clear",
+                "properties": {}
+              },
+              "supported_document": {
+                "result": "clear",
+                "properties": {}
+              }
+            }
+          },
+          "police_record": {
+            "result": "clear"
+          }
+        }
+      }
+    }
   }
 ]
